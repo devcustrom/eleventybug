b89480517712abc1d8ee634023b19a696a76c033
Alexey Khachaturyan <devcustrom@gmail.com>
2022-04-29 18:53:08 +0300

    I found a solution

--- src/_data/test.js
+++ src/_data/test.js
@@ -1,19 +1,18 @@
-const fields = require("../js/admin/characteristics/characteristicsFields.json")
-const characteristics = require("../js/admin/characteristics/characteristics.json")
+const { characteristicsFields } = require("../js/admin/characteristics/characteristicsFields.json")
+const { characteristics } = require("../js/admin/characteristics/characteristics.json")
 
 module.exports = () => {
-	let test = characteristics.characteristics.filter(item => {
-		let field = fields.characteristicsFields.find(i => i.name === item.type)
-
-		if(field !== undefined) {
-			item.label = field.label
-			item.type = field.type
-			item.unit = field.unit ? field.unit : false
-		} else {
-			item = false
+	let test = []
+	characteristics.forEach(item => {
+		const $item = Object.assign({}, item);
+		let field = characteristicsFields.find(i => i.name === $item.type)
+		if (field !== undefined) {
+			$item.label = field.label
+			$item.type = field.type
+			$item.unit = field.unit ? field.unit : false
+			test.push($item)
 		}
+	});
 
-		return field !== undefined ? item : false
-	})
 	return test
 }
